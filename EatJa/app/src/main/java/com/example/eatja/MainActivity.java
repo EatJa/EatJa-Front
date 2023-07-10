6801832cb16a58db7818850d3be2a274579dc42e
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-11 01:18:24 +0900

    INIT) Naver login

--- EatJa/app/src/main/java/com/example/eatja/MainActivity.java
+++ EatJa/app/src/main/java/com/example/eatja/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.eatja;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,14 +14,19 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.eatja.databinding.ActivityMainBinding;
+import com.navercorp.nid.NaverIdLoginSDK;
+import com.navercorp.nid.oauth.OAuthLoginCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private NaverIdLoginSDK naverIdLoginSDK;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -32,6 +40,46 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // login instance 초기화
+        naverIdLoginSDK = NaverIdLoginSDK.INSTANCE;
+        naverIdLoginSDK.initialize(
+                this,
+                getString(R.string.naver_client_id),
+                getString(R.string.naver_client_secret),
+                getString(R.string.app_name)
+        );
+
+        OAuthLoginCallback oauthLoginCallback = new OAuthLoginCallback() {
+            @Override
+            public void onSuccess() {
+                // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+                android.util.Log.i("LOG", naverIdLoginSDK.getAccessToken());
+                android.util.Log.i("LOG", naverIdLoginSDK.getRefreshToken());
+                android.util.Log.i("LOG", ""+ naverIdLoginSDK.getExpiresAt());
+                android.util.Log.i("LOG", naverIdLoginSDK.getTokenType());
+                android.util.Log.i("LOG", naverIdLoginSDK.getState().toString());
+            }
+
+            @Override
+            public void onFailure(int httpStatus, String message) {
+                String errorCode = naverIdLoginSDK.getLastErrorCode().getCode();
+                String errorDescription = naverIdLoginSDK.getLastErrorDescription();
+                Toast.makeText(context, "errorCode: " + errorCode + ", errorDesc: " + errorDescription, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(int errorCode, String message) {
+                onFailure(errorCode, message);
+            }
+        };
+
+        naverIdLoginSDK.authenticate(context, oauthLoginCallback);
+
     }
+
+//    private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+//
+//    }
 
 }
