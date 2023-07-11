4f7914d41d90f3ca0212fefa347353c484538ba9
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-11 19:47:53 +0900

    FEAT) search and show markers

--- EatJa/app/src/main/java/com/example/eatja/ui/home/HomeFragment.java
+++ EatJa/app/src/main/java/com/example/eatja/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.eatja.ui.home;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,12 +21,17 @@ import com.example.eatja.MainActivity;
 import com.example.eatja.R;
 import com.example.eatja.databinding.FragmentHomeBinding;
 import com.example.eatja.search.SearchLocal;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.LatLngBounds;
+import com.naver.maps.geometry.Tm128;
+import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.LocationOverlay;
+import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
 import org.json.JSONArray;
@@ -34,6 +40,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
@@ -54,6 +61,8 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     ArrayList<Integer> tagList = new ArrayList<>();
     String[] followArray = new String[4];
     String[] tagArray = new String[4];
+
+    private ArrayList<Marker> markerArrayList = new ArrayList<>();
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -284,7 +293,10 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
                                 JSONObject item = items.getJSONObject(i);
                                 android.util.Log.i("Item!", item.toString());
                             }
-                            displaySearchedMarkers(items);
+
+                            // Call displaySearchedMarkers on the main thread
+                            mainActivity.runOnUiThread(() -> displaySearchedMarkers(items));
+//                            displaySearchedMarkers(items);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -293,6 +305,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
                     }
                 }).start();
 
+//                displaySearchedMarkers(itemsArray);
                 searchView.clearFocus();
 
                 return true;
@@ -323,6 +336,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(@NonNull NaverMap nMap) {
         naverMap = nMap;
 
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true);
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BICYCLE, false);
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_CADASTRAL, false);
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_MOUNTAIN, false);
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRAFFIC, false);
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, false);
+
         uiSettings = naverMap.getUiSettings();
         setUiSettings();
 
@@ -342,6 +362,46 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     public void displaySearchedMarkers(JSONArray array) {
 
+        ArrayList<LatLng> latLngArrayList = new ArrayList<>();
+        if (!markerArrayList.isEmpty()) {
+            for (int i = 0 ; i < markerArrayList.size() ; i++) {
+                markerArrayList.get(i).setMap(null);
+            }
+            markerArrayList = new ArrayList<>();
+        }
+        for (int i = 0 ; i < array.length() ; i ++) {
+            try {
+                JSONObject item = array.getJSONObject(i);
+
+                Integer mapx = item.getInt("mapx");
+                Integer mapy = item.getInt("mapy");
+                Tm128 tmPosition = new Tm128(mapx, mapy);
+                LatLng latLng = tmPosition.toLatLng();
+                latLngArrayList.add(latLng);
+
+                String title = item.getString("title");
+
+                Marker marker = new Marker();
+                marker.setPosition(latLng);
+                marker.setCaptionText(title);
+                marker.setCaptionHaloColor(Color.WHITE);
+                marker.setCaptionTextSize(14);
+
+                markerArrayList.add(marker);
+
+                marker.setMap(naverMap);
+
+            } catch (JSONException e) {
+                android.util.Log.e("JSONError", "displaySearchedMarkers error");
+                e.printStackTrace();
+            }
+        }
+
+        LatLngBounds bounds = new LatLngBounds.Builder()
+                .include(latLngArrayList)
+                .build();
+        CameraUpdate cameraUpdate = CameraUpdate.fitBounds(bounds, 250);
+        naverMap.moveCamera(cameraUpdate);
     }
     @Override
     public void onDestroyView() {
