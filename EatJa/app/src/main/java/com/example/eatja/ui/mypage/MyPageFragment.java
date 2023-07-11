954e33737f0433a045bc115d2a31d8c791503e76
ryuihchae <ihchaeryu@kaist.ac.kr>
2023-07-12 06:14:05 +0900

    FEAT) get profileimg and name

--- EatJa/app/src/main/java/com/example/eatja/ui/mypage/MyPageFragment.java
+++ EatJa/app/src/main/java/com/example/eatja/ui/mypage/MyPageFragment.java
@@ -1,20 +1,39 @@
 package com.example.eatja.ui.mypage;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.eatja.MainActivity;
 import com.example.eatja.databinding.FragmentMypageBinding;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MyPageFragment extends Fragment {
 
     private FragmentMypageBinding binding;
+    private MainActivity mainActivity;
+    private JSONObject jsonObject;
+    private TextView userNameTV;
+    private CircleImageView profileIV;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +43,31 @@ public class MyPageFragment extends Fragment {
         binding = FragmentMypageBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMypage;
-        myPageViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // get profile json
+        jsonObject = mainActivity.getJsonObject();
+
+        // bind views
+        userNameTV = binding.usernameTextView;
+        profileIV = binding.userProfileImageView;
+
+        // set profile
+        setProfile();
+
         return root;
+    }
+
+    public void setProfile() {
+        try {
+            String userName = jsonObject.getString("userName");
+            userNameTV.setText(userName);
+            String profileUrl = jsonObject.getString("profileImg");
+            Picasso.get()
+                    .load(profileUrl)
+                    .into(profileIV);
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
